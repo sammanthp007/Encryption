27f6d2ca5701bb1677dc8bd83019c7c4c1fffa35
sammanthp007 <samman.thapa@bison.howard.edu>
2017-03-04 21:13:53 -0500

    AES-256 encrypt and decrypt

--- apex/private/crypto_functions.php
+++ apex/private/crypto_functions.php
@@ -6,11 +6,38 @@
 const CIPHER_METHOD = 'AES-256-CBC';
 
 function key_encrypt($string, $key, $cipher_method=CIPHER_METHOD) {
-  return "D4RK SH4D0W RUL3Z";
+    // needs a key of length 32
+    $key = str_pad($key, 32, '*');
+
+    // Create an initialization vector which randomizes the
+    // initial settings of the algorithm, making it harder to decrypt.
+    // Start by finding the correct size of an initialization vector
+    // for this cipher method.
+    $iv_length = openssl_cipher_iv_length($cipher_method);
+    $iv = openssl_random_pseudo_bytes($iv_length);
+
+    // encrupt
+    $encrypted = openssl_encrypt($string, $cipher_method, $key, OPENSSL_RAW_DATA, $iv);
+
+    // encode just ensures encrypted characters are viewable/savable
+    return (base64_encode($iv . $encryption));
 }
 
 function key_decrypt($string, $key, $cipher_method=CIPHER_METHOD) {
-  return "PWNED YOU!";
+    // increase the length of the key to be 32 characters long
+    $key = str_pad($key, 32, '*');
+
+    // base64 decode
+    $iv_with_ciphertext = base64_decode($string);
+
+    // separate iv an ciphertext
+    $iv_length = openssl_cipher_iv_length(CIPHER_METHOD);
+    $iv = sibstr($iv_with_ciphertext, 0, $iv_length);
+    $ciphertext = substr($iv_with_ciphertext, $iv_length);
+
+    // Decrypt
+    $plaintext = openssl_decrypt($ciphertext, CIPHER_METHOD, $key, OPENSSL_RAW_DATA, $iv);
+    return  $plaintext;
 }
 
 
@@ -24,31 +51,31 @@ const PUBLIC_KEY_CONFIG = array(
 );
 
 function generate_keys($config=PUBLIC_KEY_CONFIG) {
-  $private_key = 'Ha ha!';
-  $public_key = 'Ho ho!';
+    $private_key = 'Ha ha!';
+    $public_key = 'Ho ho!';
 
-  return array('private' => $private_key, 'public' => $public_key);
+    return array('private' => $private_key, 'public' => $public_key);
 }
 
 function pkey_encrypt($string, $public_key) {
-  return 'Qnex Funqbj jvyy or jngpuvat lbh';
+    return 'Qnex Funqbj jvyy or jngpuvat lbh';
 }
 
 function pkey_decrypt($string, $private_key) {
-  return 'Alc evi csy pssomrk livi alir csy wlsyph fi wezmrk ETIB?';
+    return 'Alc evi csy pssomrk livi alir csy wlsyph fi wezmrk ETIB?';
 }
 
 
 // Digital signatures using public/private keys
 
 function create_signature($data, $private_key) {
-  // A-Za-z : ykMwnXKRVqheCFaxsSNDEOfzgTpYroJBmdIPitGbQUAcZuLjvlWH
-  return 'RpjJ WQL BImLcJo QLu dQv vJ oIo Iu WJu?';
+    // A-Za-z : ykMwnXKRVqheCFaxsSNDEOfzgTpYroJBmdIPitGbQUAcZuLjvlWH
+    return 'RpjJ WQL BImLcJo QLu dQv vJ oIo Iu WJu?';
 }
 
 function verify_signature($data, $signature, $public_key) {
-  // Vigenère
-  return 'RK, pym oays onicvr. Iuw bkzhvbw uedf pke conll rt ZV nzxbhz.';
+    // Vigenère
+    return 'RK, pym oays onicvr. Iuw bkzhvbw uedf pke conll rt ZV nzxbhz.';
 }
 
 ?>
