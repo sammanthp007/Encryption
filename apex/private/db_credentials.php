736e0b376bd79b3e7e22fba0dc581611ff6747fe
sammanthp007 <samman.thapa@bison.howard.edu>
2017-03-04 20:42:01 -0500

    updated db_credentials for real

--- apex/private/db_credentials.php
+++ apex/private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
   define("DB_SERVER", "localhost");
   define("DB_USER", "root");
-  define("DB_PASS", "");
-  define("DB_NAME", "apex");
+  define("DB_PASS", "password");
+  define("DB_NAME", "assignment04");
 ?>
